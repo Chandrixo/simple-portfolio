9b39e64a2d93fe9faaf792ef582ad1edff9687a1
Chandrika Saga <149573599+Chandrixo@users.noreply.github.com>
2025-04-07 11:52:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('contact-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Thank you for your message!');
+    // Additional functionality can be added to send form data to your email or a backend server
+  });
+  
